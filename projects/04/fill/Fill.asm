6610b148e4a7a3133c74a25102623e24b75d3c27
aemonk <aemonk986@gmail.com>
2020-09-01 21:14:36 +0200

    mult done

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,46 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+	
+	@SCREEN
+	D=A
+	@addr
+	M=D // addr = 16384 screen memory map
+
+	@8192 // height
+	D=A
+	@n
+	M=D // n = RAM[0]
+
+	@i
+	M=0 // i = 
+
+	// RAM[24576 KBD address
+	// if KBD > 0 then 1
+	// else 0
+
+(LOOP)
+	@i
+	D=M
+	@n
+	D=D-M
+	@END
+	D;JGT // if i > n goto END
+	
+	@addr
+	A=M
+	M=-1 // RAM[addr] = 1111...
+
+	@i 
+	M=M+1 // i = i + 1
+	@1 // 
+	D=A
+	@addr
+	M=D+M // addr = addr + 1
+	@LOOP
+	0;JMP // goto LOOP
+
+(END)
+	@END // programs end
+	0;JMP // infinite loop
